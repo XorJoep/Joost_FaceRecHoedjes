6a8a75e28f1791e568544f3bc1317ef704180a0e
Joep Dumont <joep@dumont.nl>
2020-01-22 11:20:22 +0100

    final version v3

--- Blur.cpp
+++ Blur.cpp
@@ -39,7 +39,9 @@ inline uint32_t blur(uint32_t window[WINDOW_SIZE][WINDOW_SIZE],
 	uint16_t sum_green = 0;
 	uint16_t sum_blue = 0;
 	for (uint8_t i = -HALF_SIZE; i <= HALF_SIZE; i++)
+		#pragma HLS PIPELINE
 		for (uint8_t j = -HALF_SIZE; j <= HALF_SIZE; j++)
+			#pragma HLS PIPELINE
 			if (within_face(x + i, y + j, face_x, face_y, face_w, face_h)) {
 				uint8_t k_val = kernel[i + HALF_SIZE][j + HALF_SIZE];
 				uint32_t pixel = window[i + HALF_SIZE][j + HALF_SIZE];
@@ -51,20 +53,20 @@ inline uint32_t blur(uint32_t window[WINDOW_SIZE][WINDOW_SIZE],
 }
 
 
-
-
 void face_blurrer(pixel_stream &src, pixel_stream &dst, 
 	uint16_t face_x, uint16_t face_y, uint16_t face_w, uint16_t face_h, 
 	uint8_t switch_is_on)
 {
-#pragma HLS INTERFACE ap_ctrl_none port=return
-#pragma HLS INTERFACE axis port=&src
-#pragma HLS INTERFACE axis port=&dst
-#pragma HLS INTERFACE s_axilite port=face_x
-#pragma HLS INTERFACE s_axilite port=face_y
-#pragma HLS INTERFACE s_axilite port=face_w
-#pragma HLS INTERFACE s_axilite port=face_h
-#pragma HLS INTERFACE s_axilite port=switch_is_on
+	#pragma HLS INTERFACE ap_ctrl_none port=return
+	#pragma HLS INTERFACE axis port=&src
+	#pragma HLS INTERFACE axis port=&dst
+	#pragma HLS INTERFACE s_axilite port=face_x
+	#pragma HLS INTERFACE s_axilite port=face_y
+	#pragma HLS INTERFACE s_axilite port=face_w
+	#pragma HLS INTERFACE s_axilite port=face_h
+	#pragma HLS INTERFACE s_axilite port=switch_is_on
+
+	#pragma HLS PIPELINE
 
 	// Data to be stored across 'function calls'
 	static uint16_t x = 0;
@@ -72,67 +74,76 @@ void face_blurrer(pixel_stream &src, pixel_stream &dst,
 
 	uint32_t pixel_in;
 	uint32_t pixel_out;
-	uint32_t line_buf[WINDOW_SIZE - 1][WIDTH]; // matrix buffering pixeldata
 
-	  // Load initial values into line buffer
+	static uint32_t line_buf[WINDOW_SIZE - 1][WIDTH]; // matrix buffering pixeldata
+	static uint32_t window[WINDOW_SIZE][WINDOW_SIZE];
+	static uint32_t right[WINDOW_SIZE];
+	#pragma HLS ARRAY_PARTITION variable=line_buf complete dim=1
+	#pragma HLS ARRAY_PARTITION variable=window complete dim=0
+	#pragma HLS ARRAY_PARTITION variable=right complete
+
+		// Load initial values into line buffer
 	uint32_t read_count = WIDTH * HALF_SIZE + HALF_SIZE + 1;
-	buf_x1 : for (int x = WIDTH - HALF_SIZE - 1; x < WIDTH; x++)
-		#pragma HLS PIPELINE
-		line_buf[HALF_SIZE - 1][x] = src.read();
-		buf_y : for (int y = HALF_SIZE; y < WIN_SIZE - 1; y++)
-			buf_x2 : for (int x = 0; x < WIDTH; x++)
-				#pragma HLS PIPELINE
-				// line_buf[y][x] = in_stream.read();
-				src >> line_buf[y][x]
-
 
 	pixel_data p_in;
+	pixel_data p_out;
 
-	// Load input data from source
 	src >> p_in;
+	pixel_in = p_in.data;
 
-	////////////////////////////////
+	if (y < WINDOW_SIZE - 1) { // Fill a buffer for the window
+		
+		line_buf[y][x] = pixel_in;
+		pixel_out = pixel_in;
+	}
+	else {
 
-	// Pixel data to be stored across 'function calls'
-	// static pixel_data p_out = p_in;
-	//static uint32_t dl = 0;
-	//static uint32_t dc = 0;
+		// Compute outgoing pixel data
+		switch (switch_is_on) {
+			case 0:
+				pixel_out = pixel_in;
+				break;
+			case 1:
+				if (within_face(x, y, face_x, face_y, face_w, face_y)) {
+					pixel_out = blur(window, x, y)
+				}
+				break;
+			case 2:
+				// Horizontale pixel out
+				break;
+			case 3:
+				// pixelate blok
+				break;
+			case 4:
+				// blok pixel met blur
+				break;
+			default:
+				break;
+		}
 
-	// Current (incoming) pixel data
-	uint32_t pixel_in = p_in.data;
+		// Shift line buffer column up
+		right[0] = line_buf[0][x];
+		for (int y = 1; y < WINDOW_SIZE - 1; y++) {
+			right[y] = line_buf[y - 1][x] = line_buf[y][x];
+		}
 
-	// Compute outgoing pixel data
-	swith (mode) {
-		case 0:
-			pixel_out = pixel_in;
-			break;
-		case 1:
-			if (within_face(x, y, face_x, face_y, face_w, face_y)) {
-				pixel_out = blur(window, x, y)
+		right[WINDOW_SIZE - 1] = line_buf[WINDOW_SIZE - 2][x] = pixel_in;
+
+		// Shift window left
+		for (int y = 0; y < WINDOW_SIZE; y++) {
+			for (int x = 0; x < WINDOW_SIZE - 1; x++) {
+				window[y][x] = window[y][x + 1];
 			}
-			break;
-		case 2:
-			// Horizontale pixel out
-			break;
-		case 3:
-			// pixelate blok
-			break;
-		case 4:
-			// blok pixel met blur
-			break;
-		default:
-			break;
+		}
+
+		// Update rightmost window values
+		for (int y = 0; y < WINDOW_SIZE; y++) {
+			window[y][WINDOW_SIZE - 1] = right[y];
+		}
 	}
 
-	p_out.data = dn;
-
-	////////////////////////////////
-
-	// Write pixel to destination
+	p_out.data = pixel_out;
 	dst << p_out;
-
-	// Copy previous pixel data to next output pixel
-	p_out = p_in;
 
 	// Increment X and Y counters
 	if (p_in.last)
@@ -142,4 +153,4 @@ void face_blurrer(pixel_stream &src, pixel_stream &dst,
 	}
 	else
 			x++;
-}
+	}
